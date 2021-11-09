d1cfe8a347cddb960ceee853460bca7e35d3d88e
Huy Viet Nguyen <90419850+UiVieto@users.noreply.github.com>
2021-11-08 19:07:51 -0500

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -8,11 +8,16 @@ import numpy as np
 
 # TODO: Définissez vos fonctions ici (il en manque quelques unes)
 def linear_values() -> np.ndarray:
-    return np.array([])
+    return np.linspace(-1.3, 2.5, 64)
 
 
 def coordinate_conversion(cartesian_coordinates: np.ndarray) -> np.ndarray:
-    return np.array([])
+
+    coordonnees_polaires = []
+    for elem in cartesian_coordinates:
+        coordonnees_polaires.append([np.sqrt(elem[0]**2 + elem[1]**2), np.degrees(np.arctan(elem[1]/elem[0]))])
+
+    return np.array(coordonnees_polaires)
 
 
 def find_closest_index(values: np.ndarray, number: float) -> int:
